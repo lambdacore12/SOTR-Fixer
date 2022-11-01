45390c73b5f9b71d549ace9d43f8b35038aadf9b
Anass SABIRI <lambdacore_12@hotmail.com>
2022-11-01 14:57:43 +0100

    Added fileName and filePath

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,36 @@ namespace SOTR_Fixer
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Variables that will be used throughout the code
+        string fileName = "";
+        string filePath = "";
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region Open original file
+        private void Open_Original_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = ""; // Default file name
+            dialog.DefaultExt = ".srt"; // Default file extension
+            dialog.Filter = "SubRip file (.srt)|*.srt"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                fileName = dialog.FileName;
+                filePath = System.IO.Path.GetFileName(fileName);
+                Original_TBox.Text = filePath;
+            }
+        } 
+        #endregion
     }
 }
