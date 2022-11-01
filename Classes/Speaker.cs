473da8a89eae2a2b56df4d189337d7e93f2867f8
Anass SABIRI <lambdacore_12@hotmail.com>
2022-11-01 12:39:43 +0100

    Added a class to determine speaker info

---
+++ Classes/Speaker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTR_Fixer.Classes
+{
+    internal class Speaker
+    {
+        public string Shortcut { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
