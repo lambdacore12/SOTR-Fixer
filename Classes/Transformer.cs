e809abb1954521e285720c78dc3b925e5dbb2913
Anass SABIRI <lambdacore_12@hotmail.com>
2023-04-18 14:26:43 +0000

    Finally not yet

--- Classes/Transformer.cs
+++ Classes/Transformer.cs
@@ -10,46 +10,69 @@ namespace SOTR_Fixer.Classes
     {
         public static string Transform(string path)
         {
-            string[] strArray1 = new string[4]
+            string[] shortcuts = new string[3]
             {
                 "j",
                 "l",
-                "v",
-                "t"
+                "v"
             };
-            string[] strArray2 = new string[3]
+
+            string[] names = new string[3]
             {
                 " John Mueller: ",
                 " Lizzie Sassmann: ",
                 " Vanessa Fox: "
             };
+
             List<SrtItems> srtItemsList = Extractor.Extract(path);
-            for (int index1 = 0; index1 < srtItemsList.Count; ++index1)
+
+            //loop through all srt items
+            for (int i = 0; i < srtItemsList.Count; ++i)
             {
-                string lower = srtItemsList[index1].SubLine[0][0].ToString().ToLower();
-                string str1 = srtItemsList[index1].SubLine[0][1].ToString();
-                for (int index2 = 0; index2 < strArray1.Length; ++index2)
-                {
-                    if (lower == strArray1[index2] && str1 == " ")
+                // this is the key letter to be checked
+                string keyLetter = srtItemsList[i].SubLine[0][0].ToString().ToLower();
+
+                //this is the space after after the key letter
+                string keySpace = srtItemsList[i].SubLine[0][1].ToString();
+
+                //loop through shortcuts to check if they exist
+                for (int j = 0; j < shortcuts.Length; ++j)
+                {   
+                    //if they do exist
+                    if (keyLetter == shortcuts[j] && keySpace == " ")
                     {
-                        if (lower == "t")
+                        //if the key letter is 't', meaning a time indicator
+                        if (keyLetter == "t")
                         {
-                            string str2 = "[" + srtItemsList[index1].SubStart.ToString().Substring(0, 8) + "]" + srtItemsList[index1].SubLine[0].Remove(0, 1);
-                            srtItemsList[index1].SubLine[0] = str2;
+                            //replace the key letter t in first line with "[substart0->8] (which extracts the time values)
+                            string newLine = "[" + srtItemsList[i].SubStart.ToString().Substring(0, 8) + "]" + srtItemsList[i].SubLine[0].Remove(0, 1);
+                            srtItemsList[i].SubLine[0] = newLine;
                         }
                         else
                         {
-                            string str3 = "[" + srtItemsList[index1].SubStart.ToString().Substring(0, 8) + "]";
-                            string str4 = srtItemsList[index1].SubLine[0].Remove(0, 1);
-                            string str5 = str3 + strArray2[index2] + str3 + str4;
-                            srtItemsList[index1].SubLine[0] = str5;
+                            //same as above but the desired format of [time] name [time]
+                            string time = "[" + srtItemsList[i].SubStart.ToString().Substring(0, 8) + "]";
+                            string cleanLinestr4 = srtItemsList[i].SubLine[0].Remove(0, 1);
+                            string str5 = time + names[j] + time + cleanLinestr4;
+                            srtItemsList[i].SubLine[0] = str5;
                         }
                     }
                 }
             }
-            string str = (string)null;
-            foreach (SrtItems srtItems in srtItemsList)
-                str = str + srtItems.LineCount.ToString() + Environment.NewLine + srtItems.SubStart + " --> " + srtItems.SubEnd + Environment.NewLine + srtItems.SubLine[0] + Environment.NewLine + Environment.NewLine;
+
+            //create one string to hold all of the subtitle content
+            string str = "";
+            for (int k = 0; k < srtItemsList.Count; k++)
+            {
+                str += srtItemsList[k].LineCount + Environment.NewLine;
+                str += srtItemsList[k].SubStart + " --> " + srtItemsList[k].SubEnd + Environment.NewLine;
+                for (int z = 0; z < srtItemsList[k].SubLine.Count; z++)
+                {
+                    str += srtItemsList[k].SubLine[z] + Environment.NewLine;
+                }
+                str += Environment.NewLine;
+            }
+
             return str;
         }
     }
